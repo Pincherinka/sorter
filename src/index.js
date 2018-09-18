6b955ae6a995fd076ed236e48a240cc0907bda9c
tatsiana_shautsova <tatsiana_shautsova@epam.com>
2018-09-18 08:26:17 +0300

    Task 3

--- src/index.js
+++ src/index.js
@@ -1,30 +1,66 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array =[];
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
-  at(index) {
-    // your implementation
+  at(index) {      
+    return this.array[index];       
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
-  sort(indices) {
-    // your implementation
-  }
+  sort(indices) {   
+        
+    let subArray = [];
+    indices.forEach(index=>{
+      subArray.push(this.array[index])
+    });
+    // custom sort as set in setComporator
+    if (this.sorting != null){
+      subArray.sort(this.sorting);      
+    }
+    else {
+    // check a type of array
+      let sortAsNumber = true;
+      for (let element of subArray){
+        if (isNaN(element)===true){
+          sortAsNumber = false;
+          break;
+        }
+      };
+      // sort values according to type
+      if (sortAsNumber === true){
+      subArray.sort(function(a, b) {
+          return a - b;    
+        });
+      } else subArray.sort();  
+     
+    }   
+    // sort indexes of initial array
+    indices.sort(function(a, b) {
+      return a - b;    
+    });
 
-  setComparator(compareFunction) {
-    // your implementation
+    // insert sorted values back to initial array
+    for (let i=0; i<indices.length; i++){
+      this.array[indices[i]]=subArray[i];
+    }
+
+  return this.array;  
+  }  
+
+  setComparator(compareFunction) {    
+    this.sorting = compareFunction;       
   }
 }
 
